809bed53939229a9cbd7b328b72cfe11b381b29e
lstucker <lstucker@gitlab.com>
2022-06-15 16:06:39 -0500

    adding aws

--- lib/tanuki/app.rb
+++ lib/tanuki/app.rb
@@ -3,7 +3,7 @@
 module Tanuki
     # Sinatra application
     class App < Sinatra::Base
-  
+      set :aws, 'AKIALALEMEL372930LIA'
       set :logging, true
       set :static, true
       set :public_dir, File.expand_path('static', __dir__)
